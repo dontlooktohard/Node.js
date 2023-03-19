4bbe1e31c0e17050d03c4dddd7a0901f6a4f5e9b
Your Name01echo <xitamarfx@gmail.com>
2023-03-19 10:39:59 +0200

    deleted node

---
+++ user/data.js
@@ -0,0 +1,6 @@
+[
+  { id: "2e95uc6tky8pyuz2", email: "benny@gmail.com", name: "benny solomon" },
+  { id: "2e95ui9nkya156nq", email: "test@gmail.com", name: "tester test" },
+  { id: "2fgj56bclf85tqv7", email: "ben@gmail.com", name: "ben" },
+  { id: "2fgj55zglf8jtsuw", email: "bb@gmail.com", name: "bb" },
+];
